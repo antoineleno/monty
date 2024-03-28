36fcc604f57bb3addf83ae4dbd8c4bfe829dd749
antoineleno <johndoe@example.com>
2024-03-28 19:46:17 +0800

    S

--- push.c
+++ push.c
@@ -7,23 +7,26 @@
  */
 void push_element(stack_t **stack, int n)
 {
-	stack_t *new_node;
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
+    new_node->n = n;
+    new_node->prev = NULL;
 
-	new_node->n = n;
-
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
-	{
-		(*stack)->prev = new_node;
-	}
-	*stack = new_node;
+    if (*stack == NULL)
+    {
+        new_node->next = NULL;
+        *stack = new_node;
+    }
+    else
+    {
+        new_node->next = *stack;
+        (*stack)->prev = new_node;
+        *stack = new_node;
+    }
 }
 
